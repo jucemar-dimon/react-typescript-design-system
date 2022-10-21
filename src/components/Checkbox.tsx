1cebbb0336889d422c5b56c14fedd6776e6d4aca
Jucemar Dimon <jucemar.dimon@gmail.com>
2022-10-21 20:13:03 -0300

    Conclui a codificação do design system

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -3,11 +3,13 @@ import { clsx } from "clsx";
 import { ReactNode } from "react";
 import { Check } from "phosphor-react";
 
-export interface CheckboxProps {}
+export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps {
+  
+}
 
 export function Checkbox(props: CheckboxProps) {
   return (
-    <CheckboxPrimitive.Root className=" w-6 h-6 p-[2px] bg-gray-800 rounded">
+    <CheckboxPrimitive.Root className=" w-6 h-6 p-[2px] bg-gray-800 rounded" {...props}>
       <CheckboxPrimitive.Indicator asChild>
         <Check weight="bold" className="w-5 h-5 text-cyan-500" />
       </CheckboxPrimitive.Indicator>
---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,42 @@
+import { SignIn } from "./SignIn";
+import { Meta, StoryObj } from "@storybook/react";
+import {within,userEvent,waitFor} from '@storybook/testing-library';
+import {expect} from '@storybook/jest';
+import {rest} from 'msw'
+
+export default {
+  title: "Pages/SignIn",
+  component: SignIn,
+  args: {},
+  argTypes: {},
+  types:{},
+  parameters:{
+    msn:{
+        handlers:[
+            rest.post('/sessions',(req,res,ctx)=>{
+                return res(ctx.json({
+                    message: 'Login realizado!'
+                }))
+            })
+        ],
+    }
+  }
+} as Meta;
+
+export const Default: StoryObj = {
+    play: async ({canvasElement})=>{
+        const canvas = within(canvasElement);
+
+        userEvent.type(canvas.getByPlaceholderText('Digite seu e-mail'),'jucemar.dimon@gmail.com');
+        userEvent.type(canvas.getByPlaceholderText('********'),'12345678');
+
+        userEvent.click(canvas.getByRole('button'));
+
+       
+
+        await waitFor(()=>{
+            return expect(canvas.getByText('Login realizado com sucesso')).toBeInTheDocument();
+        });
+
+    }
+};
